c3b377a632238309c2f450822bafd2ecd8e137de
SongMinJun <dkaskgkdua@gmail.com>
2020-12-13 22:27:45 +0900

    쥬얼리와 스톤 문제 - Set을 이용해서 for문으로 탐색

--- src/test/java/chapter1/JewelsAndStones/JewelsAndStones.java
+++ src/test/java/chapter1/JewelsAndStones/JewelsAndStones.java
@@ -1,2 +1,28 @@
-package chapter1.JewelsAndStones;public class JewelsAndStones {
+package chapter1.JewelsAndStones;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class JewelsAndStones {
+    public static void main(String[] args) {
+        String J = "aA";
+        String S = "aAAbbbb";
+        System.out.println(solve(J, S));
+    }
+    public static int solve(String jew, String stone) {
+        // jewel 담을 그릇
+        Set<Character> set = new HashSet<>();
+
+        for(char jewel : jew.toCharArray()) {
+            set.add(jewel); // a, A
+        }
+        int count = 0;
+        for(char stoneChar : stone.toCharArray()) {
+            if(set.contains(stoneChar)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
